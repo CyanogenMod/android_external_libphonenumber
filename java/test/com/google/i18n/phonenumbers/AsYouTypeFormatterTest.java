94c2440dc41689b7d9c169edf5089eeebf7527d7
Shaopeng Jia <shaopengjia@google.com>
2010-05-20 15:33:49 -0700

    Fix a bug where NANPA numbers starting with 1 were incorrectly formatted.
    Change-Id: I094e175a1512f75ac7228f1acace307455de3ad3

--- java/test/com/google/i18n/phonenumbers/AsYouTypeFormatterTest.java
+++ java/test/com/google/i18n/phonenumbers/AsYouTypeFormatterTest.java
@@ -54,6 +54,19 @@ public class AsYouTypeFormatterTest extends TestCase {
     assertEquals("650 253 2222", formatter.inputDigit('2'));
 
     formatter.clear();
+    assertEquals("1", formatter.inputDigit('1'));
+    assertEquals("16", formatter.inputDigit('6'));
+    assertEquals("165", formatter.inputDigit('5'));
+    assertEquals("1650", formatter.inputDigit('0'));
+    assertEquals("16502", formatter.inputDigit('2'));
+    assertEquals("1 650 25", formatter.inputDigit('5'));
+    assertEquals("1 650 253", formatter.inputDigit('3'));
+    assertEquals("1 650 253 2", formatter.inputDigit('2'));
+    assertEquals("1 650 253 22", formatter.inputDigit('2'));
+    assertEquals("1 650 253 222", formatter.inputDigit('2'));
+    assertEquals("1 650 253 2222", formatter.inputDigit('2'));
+
+    formatter.clear();
     assertEquals("6", formatter.inputDigit('6'));
     assertEquals("65", formatter.inputDigit('5'));
     assertEquals("650", formatter.inputDigit('0'));
