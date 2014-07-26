a89d41d8e584358ec7119489a89fcb7050416042
Jorge Ruesga <jorge@ruesga.com>
2014-07-26 02:06:54 +0200

    libphonenumber: fix German metadata
    Change DE general possibleNumberPattern metadata from {2-15} to {4-14}.
    
    JIRA: CYAN-4258
    https://jira.cyanogenmod.org/browse/CYAN-4258
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Change-Id: I0c070fe208421941674a6e44b204bdd879527b52

--- java/test/com/android/i18n/phonenumbers/PhoneNumberUtilTest.java
+++ java/test/com/android/i18n/phonenumbers/PhoneNumberUtilTest.java
@@ -176,7 +176,7 @@ public class PhoneNumberUtilTest extends TestMetadataTestCase {
     assertEquals("$1 $2 $3", metadata.getNumberFormat(5).getFormat());
     assertEquals("(?:[24-6]\\d{2}|3[03-9]\\d|[789](?:[1-9]\\d|0[2-9]))\\d{1,8}",
                  metadata.getFixedLine().getNationalNumberPattern());
-    assertEquals("\\d{2,14}", metadata.getFixedLine().getPossibleNumberPattern());
+    assertEquals("\\d{4,14}", metadata.getFixedLine().getPossibleNumberPattern());
     assertEquals("30123456", metadata.getFixedLine().getExampleNumber());
     assertEquals("\\d{10}", metadata.getTollFree().getPossibleNumberPattern());
     assertEquals("900([135]\\d{6}|9\\d{7})", metadata.getPremiumRate().getNationalNumberPattern());
