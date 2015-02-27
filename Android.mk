c51f33d22929dc599611349979d305c5854b8f85
Jean-Marie Hénaff <jmhenaff@google.com>
2015-02-27 15:00:10 +0000

    Revert "Disable Jack because of resource renaming"
    This reverts commit 990293b75cf3ee87e80f6c185706b6d9eb7c1870.
    
    Bug: 19030678
    Change-Id: I488bd5dcb3c21fbeca4d7cbbee906a8450afb5a4

--- Android.mk
+++ Android.mk
@@ -47,13 +47,7 @@ LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_JAVA_LIBRARIES := core-libart
-LOCAL_JACK_ENABLED := disabled
 include $(BUILD_STATIC_JAVA_LIBRARY)
-
-# Allow to build jack library from Jill
-$(full_classes_jack): $(full_classes_proguard_jar) $(JILL_JAR) $(JACK_JAR)
-	$(transform-jar-to-jack)
-
 
 # For unbundled use, supports gingerbread and up.
 include $(CLEAR_VARS)
