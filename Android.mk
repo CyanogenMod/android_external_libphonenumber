3bd754c0967ed003578e460faa83ad8e777a636b
Paul Duffin <paulduffin@google.com>
2015-12-11 10:34:27 +0000

    Fix build breakage caused by upgrading to 7.2.1
    Moved libprotobuf-java-nano to LOCAL_STATIC_JAVA_LIBRARIES to
    make sure that it is available to all clients.
    
    Bug: 24793335
    Change-Id: Iefb21cbd33e2bb0427f8e8687329c3bc8f519eba

--- Android.mk
+++ Android.mk
@@ -68,7 +68,7 @@ LOCAL_SRC_FILES := $(libphonenumber_src_files)
 LOCAL_JAVA_RESOURCE_DIRS := $(libphonenumber_resource_dirs)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SDK_VERSION := 9
-LOCAL_JAVA_LIBRARIES := libprotobuf-java-nano
+LOCAL_STATIC_JAVA_LIBRARIES := libprotobuf-java-nano
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 # Tests for unbundled use.
